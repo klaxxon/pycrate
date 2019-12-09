8531f11f44938008d6402656b154df75dbb88a5c
klaxxon <487591+klaxxon@users.noreply.github.com>
2019-12-09 08:28:55 -0600

    Remove test line. New naming.

--- gocode/src/tests/s1appdu_test.go
+++ gocode/src/tests/s1appdu_test.go
@@ -6,10 +6,9 @@ import (
 	"testing"
 )
 
-var enums = map[string]map[string]string{"a": {"aa": "aaa"}}
 
 func TestS1APPdu(t *testing.T) {
-	pdu1 := pdu.S1apPdu{}
+	pdu1 := pdu.S1ap_Pdu{}
 	enc := pdu1.Encode()
 	per := asn2gort.NewPERDecoderFromString(enc, false)
 	pdu2 := pdu.S1apPdu{}.S1AP_PDU
